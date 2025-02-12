027d5f44f1d5222094222dfa9fce705af8a52173
cathalst <cathalstafford1@gmail.com>
2025-02-12 12:22:38 +0000

    Show movie review excerpts; Refactor Details page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -34,7 +34,7 @@ export const getMovies = () => {
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
     )
-      .then((res) => res.jsxon())
+      .then((res) => res.json())
       .then((json) => {
         // console.log(json.results);
         return json.results;
--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,3 +1,5 @@
+
+import MovieReviewPage from "./pages/movieReviewPage";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
@@ -21,6 +23,8 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+
       </Routes>
     </BrowserRouter>
   );
